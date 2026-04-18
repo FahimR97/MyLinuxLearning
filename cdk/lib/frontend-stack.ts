084023e5a734b56edd8c01a0bf3d0608028476e1
FahimR97 <fahimrayhan97@gmail.com>
2026-04-18 19:11:48 +0100

    feat: add basic auth to CloudFront (login required)

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+
 export class FrontendStack extends cdk.Stack {
   public readonly bucket: s3.Bucket;
   public readonly distribution: cloudfront.Distribution;
@@ -16,10 +17,33 @@ export class FrontendStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
+    const authFunction = new cloudfront.Function(this, 'BasicAuthFunction', {
+      code: cloudfront.FunctionCode.fromInline(`
+function handler(event) {
+  var request = event.request;
+  var headers = request.headers;
+  var authString = "Basic " + "ZmFoaW06bGludXhsYWI=";
+  if (typeof headers.authorization === "undefined" || headers.authorization.value !== authString) {
+    return {
+      statusCode: 401,
+      statusDescription: "Unauthorized",
+      headers: { "www-authenticate": { value: "Basic realm=\\"Linux Learning Lab\\"" } }
+    };
+  }
+  return request;
+}
+      `.trim()),
+      runtime: cloudfront.FunctionRuntime.JS_2_0,
+    });
+
     this.distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
         origin: origins.S3BucketOrigin.withOriginAccessControl(this.bucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        functionAssociations: [{
+          function: authFunction,
+          eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+        }],
       },
       defaultRootObject: 'index.html',
       errorResponses: [
